5f165fb78b51c3797783ffb55c1aa43b5dae5c30
Feller <iamliurenjie@gmail.com>
2022-03-17 13:50:57 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-fetch("https://api.muxiaoguo.cn/api/163reping")
+fetch("https://api.muxiaoguo.cn/api/163reping?api_key=f21d80c84331c877")
 	.then((response) => response.json())
 	.then((data) => {
 		document.getElementById("content").innerHTML = data.data.content;
